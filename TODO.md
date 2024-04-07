c2a6cdef3b3fc2e11b7bad1bdb784fc8a9d67067
George Paci <george.paci@excella.com>
2024-04-07 13:48:49 -0400

    Simple parsing of lines; minor change to input syntax

--- TODO.md
+++ TODO.md
@@ -2,3 +2,6 @@
 
  __ make the language case-insensitive  04/07/24 13:13:42 EDT
  __ if something is an int, emit it as an int, not a float with six trailing 0 digits  04/07/24 13:20:20 EDT
+
+# DONE
+ ++ take a line and turn it into a dict  04/07/24 13:27:43 EDT  d. 04/07/24 13:47:25 EDT
--- diagram.py
+++ diagram.py
@@ -4,7 +4,7 @@ INPUT='''
 color blue
 rect A "Starting_Point" center 100 100 size 100 50
 rrect B "Ending_Point" ul 300 300 size 100 150 color green
-edge E from A lr to B
+edge E from A.lr to B
 '''
 
 SVG_TEMPLATE='''\
@@ -16,12 +16,40 @@ RECT_TEMPLATE='<rect x="%f" y="%f" width="%f" height="%f" rx="%f" ry="%f" fill="
 
 context = { 'color': None, 'edge_color': None, 'node_color': None }
 
+def to_number(s):
+    if s.isnumeric():
+        return int(s)
+    return float(s)
+
 def tokenize(s):
     # later, respect quotes
     return s.split()
 
+def parse(tokens):
+    r = {}
+    r['list'] = []
+    r['name'] = tokens[0]
+    r['id'] = tokens[1]
+    i = 2
+    while i < len(tokens):
+        current_token = tokens[i]
+        # special so far: center size ul ll ur lr color from to
+        if current_token in ['center', 'size', 'ul', 'll', 'ur', 'lr']:
+            r[current_token] = (to_number(tokens[i+1]), to_number(tokens[i+2]))
+            i += 3
+        elif current_token in ['color', 'from', 'to']:
+            r[current_token] = tokens[i+1]
+            i += 2
+        else:
+            r['list'].append(current_token)
+            i += 1
+    return r
+
 def make_object(s):
     tokens = tokenize(s)
+    print('tokens:',tokens)
+    parsed = parse(tokens)
+    print('parsed:',parsed)
     t0 = tokens[0]
     if t0 == 'color':
         context['color'] = tokens[1]
